0098d68d9757c954bee6b058c5594ce675d1b420
geiselrocha <geiselrocha@hotmail.com>
2023-02-08 22:49:07 -0300

    created the fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,21 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+            )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except (requests.ReadTimeout):
+        return None
 
 
 # Requisito 2
