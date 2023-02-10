2050370b283f8cfcbee61c9715eb3f3c66bf7f2f
geiselrocha <geiselrocha@hotmail.com>
2023-02-09 22:56:40 -0300

    test: created search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    result = []
+
+    for new in news:
+        if title.lower() in new["title"].lower():
+            result.append((new["title"], new["url"]))
+
+    return result
 
 
 # Requisito 8
