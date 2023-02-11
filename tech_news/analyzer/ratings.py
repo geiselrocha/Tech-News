d7be520194269be19d13ca75ccedd42d344c1def
geiselrocha <geiselrocha@hotmail.com>
2023-02-10 23:19:55 -0300

    feat: created top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,16 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_news = search_news({})
+
+    result = []
+
+    for new in all_news:
+        result.append(new["category"])
+    category_counts = Counter(result).most_common()
+    categories_sorted = sorted(category_counts, key=lambda x: (-x[1], x[0]))
+
+    return [category[0] for category in categories_sorted[:5]]
