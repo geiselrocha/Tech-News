3689d9b6f80abfb8488a3f2b9b2bc65c23b7871e
geiselrocha <geiselrocha@hotmail.com>
2023-02-11 19:03:33 -0300

    feat: created analyzer_menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,18 @@
+import sys
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    try:
+        menu = input(
+            "Selecione uma das opções a seguir:\n"
+            " 0 - Popular o banco com notícias;\n"
+            " 1 - Buscar notícias por título;\n"
+            " 2 - Buscar notícias por data;\n"
+            " 3 - Buscar notícias por categoria;\n"
+            " 4 - Listar top 5 categorias;\n"
+            " 5 - Sair.\n"
+        )
+        print(menu)
+    except Exception:
+        return sys.stderr.write("Opção inválida")
